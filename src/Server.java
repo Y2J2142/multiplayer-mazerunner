47c2315fd9a0c94a49e79ac445d3f89d9a2fcc22
Y2j2142 <Y2J2142@hotmail.com>
2018-02-04 15:57:43 +0100

    Server class created

---
+++ src/Server.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	static Maze maze = new Maze(21,21);
+	
+	public static void main(String[] args)
+	{
+		maze.makePath(1,1);
+	
+			int id = 0;
+			ServerSocket server;
+			try {
+				server = new ServerSocket(Integer.parseInt(args[0]));
+			
+			while(true)
+			{
+				Socket client = server.accept();
+				ThreadHandler handler = new ThreadHandler(client, id++, maze);
+				handler.start();
+			}
+			} catch (NumberFormatException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+
+}
