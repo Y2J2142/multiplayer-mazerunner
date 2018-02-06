2e1c4e2dfb1701a9e9075a3b63c7b658467c6ca7
Y2j2142 <Y2J2142@hotmail.com>
2018-02-06 14:57:12 +0100

    Move methods added

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,40 @@
 
 public class Player {
-
+	int x;
+	int y;
+	int id;
+	Maze maze;
+	
+	Player(int x, int y, int id, Maze maze)
+	{
+		this.y = x;
+		this.x = y;
+		this.id = id;
+		this.maze = maze;
+	}
+	
+	
+	public void moveLeft()
+	{
+		if(!maze.maze[x-1][y].isWall())
+			x = x - 1;
+	}
+	
+	public void moveRight()
+	{
+		if(!maze.maze[x+1][y].isWall())
+			x = x + 1;
+	}
+	
+	public void moveUp()
+	{
+		if(!maze.maze[x][y-1].isWall())
+			y = y - 1;
+	}
+	
+	public void moveDown()
+	{
+		if(!maze.maze[x][y+1].isWall())
+			y = y + 1;
+	}
 }
