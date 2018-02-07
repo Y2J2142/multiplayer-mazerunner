a73f416722e053a317860a4e22f4e5262ceb75d0
Unknown <Y2J2142@hotmail.com>
2018-02-07 15:28:11 +0100

    Move methods fixed in Player class

--- src/Player.java
+++ src/Player.java
@@ -15,27 +15,28 @@ public class Player {
 	
 	public int getX(){return x;}
 	public int getY(){return y;}
+	public int getID(){return id;}
 	public void moveLeft()
-	{
-		if(!maze.maze[x-1][y].isWall())
-			x = x - 1;
-	}
-	
-	public void moveRight()
-	{
-		if(!maze.maze[x+1][y].isWall())
-			x = x + 1;
-	}
-	
-	public void moveUp()
 	{
 		if(!maze.maze[x][y-1].isWall())
 			y = y - 1;
 	}
 	
-	public void moveDown()
+	public void moveRight()
 	{
 		if(!maze.maze[x][y+1].isWall())
 			y = y + 1;
+	}
+	
+	public void moveUp()
+	{
+		if(!maze.maze[x-1][y].isWall())
+			x = x - 1;
+	}
+	
+	public void moveDown()
+	{
+		if(!maze.maze[x+1][y].isWall())
+			x = x + 1;
 	}
 }
