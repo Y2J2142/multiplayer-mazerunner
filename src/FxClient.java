14596b50455505c6c61fc2f39ca0f9cc946f4c23
Unknown <Y2J2142@hotmail.com>
2018-02-08 02:32:06 +0100

    Graphical representation of maze added

--- src/FxClient.java
+++ src/FxClient.java
@@ -7,17 +7,22 @@ import java.net.Socket;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.GraphicsContext;
 
 public class FxClient extends Application {
 
     PrintWriter out;
     Text text = new Text();
+    Canvas canvas;
 
     public static void main(String[] args) {
         launch(args);
@@ -31,72 +36,77 @@ public class FxClient extends Application {
             Socket socket = new Socket(InetAddress.getLocalHost(), 5566);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            String fromServer = new String();
-            StackPane anchor = new StackPane();
-            Scene scene = new Scene(anchor, 100, 100);
-            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            StackPane stack = new StackPane();
+            canvas = new Canvas(1000, 1000);
+            canvas.setFocusTraversable(true);
+            out.println("l");
+            readMaze(41, in, canvas);
+            stack.getChildren().add(canvas);
+
+            Scene scene = new Scene(stack, 1000, 1000);
+            canvas.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
                 @Override
                 public void handle(KeyEvent event) {
 
                     KeyCode keyCode = event.getCode();
-                    text.setText(keyCode.toString());
-                    if (keyCode.equals(KeyCode.DOWN))
-                    {
+                    if (keyCode.equals(KeyCode.DOWN)) {
                         out.println("d");
-                        readMaze(41, in);  
-                    }
-                    else if (keyCode.equals(KeyCode.UP))
-                    {
+                        readMaze(41, in, canvas);
+                    } else if (keyCode.equals(KeyCode.UP)) {
                         out.println("u");
-                        readMaze(41, in);   
-                    }
-                    else if (keyCode.equals(KeyCode.LEFT))
-                    {
+                        readMaze(41, in, canvas);
+                    } else if (keyCode.equals(KeyCode.LEFT)) {
                         out.println("l");
-                        readMaze(41, in);           
-                    }
-                    else if (keyCode.equals(KeyCode.RIGHT))
-                    {
+                        readMaze(41, in, canvas);
+                    } else if (keyCode.equals(KeyCode.RIGHT)) {
                         out.println("r");
-                        readMaze(41, in);
+                        readMaze(41, in, canvas);
                     }
                 }
 
             });
-            anchor.getChildren().add(text);
+            stack.getChildren().add(text);
             primaryStage.setScene(scene);
             primaryStage.show();
 
-            for(int i = 0; i <=41; i++)
-            {
-                fromServer = in.readLine();
-                System.out.println(fromServer);
+            for (int i = 0; i <= 41; i++) {
+                fromServer = in.readLine(); 
             }
-            System.out.println("dupa");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-void readMaze(int size, BufferedReader in)
-{
-    for(int i = 0; i <=size; i++)
-    {
-        try{
+    void readMaze(int size, BufferedReader in, Canvas canvas) {
+        for (int i = 0; i <= size; i++) {
+            GraphicsContext gc = canvas.getGraphicsContext2D();
+            try 
+            {
+                String fromServer = new String();
+                fromServer = in.readLine();
+                for (int j = 0; j < fromServer.length(); j++) {
+                    if (fromServer.charAt(j) == 'X') {
+                        gc.setFill(Color.RED);
+                        gc.fillRect(j * 20, i * 20, 20, 20);
+                    } else if (fromServer.charAt(j) == '#') {
+                        gc.setFill(Color.BLACK);
+                        gc.fillRect(j * 20, i * 20, 20, 20);
+                    } else if (fromServer.charAt(j) == '0') {
+                        gc.setFill(Color.BLUE);
+                        gc.fillRect(j * 20, i * 20, 20, 20);
+                    } else {
+                        gc.setFill(Color.WHITE);
+                        gc.fillRect(j * 20, i * 20, 20, 20);
+                    }
 
-        String fromServer = new String();
-        fromServer = in.readLine();
-        System.out.println(fromServer);
-        }catch(IOException e)
-        {
-            e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
     }
-
-}
-
 
 }
